afa8c23ef9c92a7c4c4146b36c403e4d272ac405
DAY NIGHT DELIVERY SERVICES <daynight.ae@gmail.com>
2026-07-04 06:45:18 +0400

    Stabilize dark shell props

--- app/renderer/components/shell/AppShellPro.tsx
+++ app/renderer/components/shell/AppShellPro.tsx
@@ -32,7 +32,7 @@ export default function AppShellPro(props: AppShellProProps) {
       </div>
       <Sidebar activeTab={props.activeTab} setActiveTab={props.setActiveTab} collapsed={props.collapsed} setCollapsed={props.setCollapsed} privacyMode={props.privacyMode} language={props.language} />
       <div className="relative z-10 flex-1 flex flex-col min-w-0 overflow-hidden bg-gradient-to-br from-[#12081F]/80 via-transparent to-[#1B0E2D]/80">
-        <TopCommandBar activeTab={props.activeTab} setActiveTab={props.setActiveTab} privacyMode={props.privacyMode} setPrivacyMode={props.setPrivacyMode} searchQuery={props.searchQuery} setSearchQuery={props.setSearchQuery} onRefresh={props.onRefresh} isRefreshing={props.isRefreshing} language={props.language} />
+        <TopCommandBar activeTab={props.activeTab} setActiveTab={props.setActiveTab} privacyMode={props.privacyMode} setPrivacyMode={props.setPrivacyMode} searchQuery={props.searchQuery} setSearchQuery={props.setSearchQuery} onRefresh={props.onRefresh} isRefreshing={props.isRefreshing} />
         <main className="flex-1 overflow-y-auto bg-transparent">{props.children}</main>
       </div>
       {props.toastMessage && <div className="fixed bottom-6 left-1/2 -translate-x-1/2 z-50 p-3.5 rounded-2xl border border-white/15 bg-[#160A26]/90 text-xs text-white font-bold shadow-knoux-glow-lg">{props.toastMessage}</div>}
