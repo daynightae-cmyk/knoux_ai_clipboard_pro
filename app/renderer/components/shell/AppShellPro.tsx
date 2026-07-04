0bbbc2f2ea7cbda75504848bc242fb1b68a214e0
DAY NIGHT DELIVERY SERVICES <daynight.ae@gmail.com>
2026-07-05 00:54:55 +0400

    Restore command bar in app shell

--- app/renderer/components/shell/AppShellPro.tsx
+++ app/renderer/components/shell/AppShellPro.tsx
@@ -1,10 +1,14 @@
 import Sidebar from "./Sidebar";
+import TopCommandBar from "./TopCommandBar";
 
 export default function AppShellPro(props: any) {
   return (
     <div className="relative flex h-screen overflow-hidden knoux-app-shell">
-      <Sidebar activeTab={props.activeTab} setActiveTab={props.setActiveTab} collapsed={props.collapsed} setCollapsed={props.setCollapsed} privacyMode={false} language={props.language} />
-      <main className="relative z-10 flex-1 overflow-y-auto bg-white">{props.children}</main>
+      <Sidebar activeTab={props.activeTab} setActiveTab={props.setActiveTab} collapsed={props.collapsed} setCollapsed={props.setCollapsed} privacyMode={props.privacyMode} language={props.language} />
+      <div className="relative z-10 flex flex-1 flex-col overflow-hidden bg-white">
+        <TopCommandBar activeTab={props.activeTab} setActiveTab={props.setActiveTab} privacyMode={props.privacyMode} setPrivacyMode={props.setPrivacyMode} searchQuery={props.searchQuery} setSearchQuery={props.setSearchQuery} onRefresh={props.onRefresh} isRefreshing={props.isRefreshing} />
+        <main className="flex-1 overflow-y-auto">{props.children}</main>
+      </div>
     </div>
   );
 }
