3bc6b4dffb8871b50a4dd2f2e299e28d51a56b8a
DAY NIGHT DELIVERY SERVICES <daynight.ae@gmail.com>
2026-07-05 00:54:46 +0400

    Restore shell basic classes

--- app/renderer/components/shell/AppShellPro.tsx
+++ app/renderer/components/shell/AppShellPro.tsx
@@ -2,9 +2,9 @@ import Sidebar from "./Sidebar";
 
 export default function AppShellPro(props: any) {
   return (
-    <div>
+    <div className="relative flex h-screen overflow-hidden knoux-app-shell">
       <Sidebar activeTab={props.activeTab} setActiveTab={props.setActiveTab} collapsed={props.collapsed} setCollapsed={props.setCollapsed} privacyMode={false} language={props.language} />
-      <main>{props.children}</main>
+      <main className="relative z-10 flex-1 overflow-y-auto bg-white">{props.children}</main>
     </div>
   );
 }
