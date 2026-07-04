7c44969e1934a867f2532afe1724a1916375da5a
DAY NIGHT DELIVERY SERVICES <daynight.ae@gmail.com>
2026-07-05 00:53:53 +0400

    Test shell import

--- app/renderer/components/shell/AppShellPro.tsx
+++ app/renderer/components/shell/AppShellPro.tsx
@@ -1,3 +1,4 @@
+import Sidebar from "./Sidebar";
 export default function AppShellPro(props: any) {
-  return <div className="relative h-screen overflow-hidden knoux-app-shell text-[#241B39]">{props.children}</div>;
+  return <div>{props.children}</div>;
 }
